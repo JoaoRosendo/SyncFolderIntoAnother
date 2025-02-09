4cd7cda4e1fd46cd486177a91a385d8c53afc70a
JoaoRosendo <joao.rafael_7@hotmail.com>
2025-02-09 23:02:40 +0000

    Added logic for copying from source to replica dir

--- OneWayFolderSynchronizer.py
+++ OneWayFolderSynchronizer.py
@@ -2,12 +2,42 @@ import os
 import argparse
 import time
 import logging
+import filecmp
+import shutil
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 def sync_replica_with_source(source_path, replica_path):
-    print(f"syncing {source_path} to {replica_path}")
+    if not os.path.exists(source_path):
+        logging.info(f"The path {source_path} does not exist. Aborting program.")
+        return False
+    if not os.path.isdir(source_path):
+        logging.info(f"The path {source_path} is not a directory. Aborting program.")
+        return False
+
+    os.makedirs(replica_path, exist_ok=True)
+
+    for root, _, files in os.walk(source_path):
+        for file in files:
+            source_file = os.path.join(root, file)
+            rel_path = os.path.relpath(source_file, source_path)
+            target_file = os.path.join(replica_path, rel_path)
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+
+            should_copy = False
+            if not os.path.exists(target_file):
+                should_copy = True
+            else:
+                # Compare files with the same path and name to see if they're equal
+                if not filecmp.cmp(source_file, target_file, shallow=False):
+                    should_copy = True
+
+            if should_copy:
+                shutil.copy2(source_file, target_file)
+
+    # Missing second part where it checks if replica has files that do not exist in source
+    return True
 
 
 def main():
@@ -46,8 +76,9 @@ def main():
     )
     args = parser.parse_args()
 
-    while True:
-        sync_replica_with_source(args.source_path, args.replica_path)
+    loop = True
+    while loop == True:
+        loop = sync_replica_with_source(args.source_path, args.replica_path)
         time.sleep(args.interval)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SyncFolderIntoAnother
+
+Notes:
+Every file that is in folder 1, will be also found in folder 2, while this program runs.
+This program is built for linux file system
+
+# Verificar se replica já e igual ao source (pela hash, por exemplo)
+
+# Se nao for:
+# Ver cada um dos ficheiros no original; ver se a path existe tambem na replica e por ultimo ver se os ficheiros sao iguais
+# Se forem iguais seguir para o proximo ficheiro
+# Se nao forem iguais, rescrever ficheiro
+# Se a path nao existir, criar e copiar ficheiro
+
+# No final, percorrer todos os ficheiros no diretorio replica e ver se existem no diretorio source
+# Se nao existirem, apagar
+# Recorrentemente apagar diretorios vazios
+
+# Ter atencao a edge cases por exemplo um ficheiro que nao devia existir mas dentro dum diretorio que DEVIA existir
